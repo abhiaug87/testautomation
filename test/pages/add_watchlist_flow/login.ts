993214bf72c7fcb39922cd43e70ce425872ae808
abhiaug87 <32320124+abhiaug87@users.noreply.github.com>
2018-03-06 17:08:58 +1300

    Update login.ts

--- test/pages/add_watchlist_flow/login.ts
+++ test/pages/add_watchlist_flow/login.ts
@@ -3,7 +3,7 @@ export class LoginPage {
 
 	enterLoginCredentials = function (): void {
 
-
+                browser.click('//*[@id="LoginLink"]');
 		browser.setValue('//*[@id="page_email"]', 'kulkarniabhishek23887@gmail.com')
 			.setValue('//*[@id="page_password"]', 'ayushbose@2002');
 			
@@ -15,6 +15,8 @@ export class LoginPage {
 	}
 
 	verifyLoginPage = function (): void {
+		browser.waitForVisible('//*[@id="LoginLink"]', 10000);	
+		expect(browser.isVisible('//*[@id="LoginLink"]')).toBeTruthy();
 		browser.waitForVisible('//*[@id="page_email"]', 10000);	
 		expect(browser.isVisible('//*[@id="page_email"]')).toBeTruthy();
 		browser.waitForVisible('//*[@id="page_password"]', 10000);	
